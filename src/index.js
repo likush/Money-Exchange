2d09ae7dc2e982b63f30143be033f063efaf940f
lkush <l.kushnariova@yandex.ru>
2018-02-25 18:56:54 +0300

    Fix infinite loop

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ module.exports = function makeExchange(currency) {
     let n = 5;
     let p = 1;
 
-    while (currency && !result.error) {
+    while (currency && !result.error && currency > 0) {
         count()
     }
     console.log('1')
 
     function count() {
-        if (currency >= 10000) {
+        if (currency < 0) {
+            return result
+        } else if (currency >= 10000) {
             result.error = "You are rich, my friend! We don't have so much coins for exchange";
         } else if (currency >= h) {
             result['H'] = Math.floor(currency / h);
